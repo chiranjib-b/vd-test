7ea6d405615097b2064ea010c3ea4270cce513ca
cbhattacharjee <cbhattacharjee@Pune883.egain.na>
2018-12-24 14:47:39 +0530

    NodeJS skeleton commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+// enable cors module for cross origin request
+app.use(cors());
+// Using socket.io for emit functionality
+
+//Allow cross origin header requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+var routes = require('./routes/index');
+app.use('/', routes);
+
+app.set('port', (process.env.PORT || 8001));
+
+server.listen(app.get('port'), function() {
+    console.log('VaultDragon KeyStore started on port ' + app.get('port'));
+});
